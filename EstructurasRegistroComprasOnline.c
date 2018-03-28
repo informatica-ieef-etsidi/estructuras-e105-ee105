cbdc2249aa913117cba652622b4d7d09a6a3d35e
AnaDue99 <36692754+AnaDue99@users.noreply.github.com>
2018-03-28 19:09:42 +0200

    Update EstructurasRegistroComprasOnline.c

--- EstructurasRegistroComprasOnline.c
+++ EstructurasRegistroComprasOnline.c
@@ -12,9 +12,16 @@ struct TUsuario {
 	char contrasena[50];
 	char cuentabancaria[25];
 };
+struct TAux { //Estructura  auxiliar
+	char nombre[50];
+	char contrasena[50];
+	char cuentabancaria[25];
+};
 void main() {
-	struct TUsuario usu[NUMUSUARIOS],aux[NUMUSUARIOS];
-	int opcion, comp1,j=0,k,auxx=0;
+	struct TUsuario usu[NUMUSUARIOS];
+	struct TAux aux;
+
+	int opcion, comp1,j=0,k=0,auxx=0;
 	printf("Bienvenido a nuestra pagina de ventas online.\n");
 	Sleep(2000);
 	//Bucle que se repite hasta que se quiera salir del programa
@@ -28,22 +35,21 @@ void main() {
 			switch (opcion) {
 			case 1: //Acceder a una cuenta
 				printf("Introduzca su nombre de usuario\n");
-				scanf_s("%s", &aux[j].nombre, 50); 
+				scanf_s("%s", &aux.nombre, 50); 
 				
-					for (k = 0; k < j; k++) {
-						comp1 = strcmp(aux[j].nombre, usu[k].nombre); //compara con todos los usuarios guardados
-						if (comp1 == 0) {
-							auxx=k;//guarda en auxx el numero de usuario registrado
-								break;
-						}
-					}//Compara entre todos los usuarios
-					
+				for (k = 0; k < j; k++) {
+					comp1 = strcmp(aux.nombre, usu[k].nombre); //Compara entre todos los usuarios
 					if (comp1 == 0) {
-						printf("Bienvenido %s\n", usu[auxx].nombre);
+						break;
+					}
+				}
+							
+					if (comp1 == 0) {
+						printf("Bienvenido %s\n", usu[k].nombre);
 						do {
 							printf("Introduzca su contrasena para entrar\n");
-							scanf_s("%s", &aux[auxx].contrasena, 50);
-							comp1 = strcmp(aux[auxx].contrasena, usu[auxx].contrasena); //Compara  si es la contraseña
+							scanf_s("%s", &aux.contrasena, 50);
+							comp1 = strcmp(aux.contrasena, usu[k].contrasena); //Compara  si es la contraseña
 
 							if (comp1 == 0) {
 								printf("Contrasena correcta\n");
@@ -65,23 +71,30 @@ void main() {
 				printf("Para registrarse necesitamos varios datos sobre usted\n");
 				do {
 					printf("Introduzca su nombre de usuario\n");
-					scanf_s("%s", &aux[j].nombre, 50);
+					scanf_s("%s", &aux.nombre, 50);
 					for (k = 0; k < j; k++) {
-						comp1 = strcmp(aux[j].nombre, usu[k].nombre); //Compara entre todos los usuarios
+						comp1 = strcmp(aux.nombre, usu[k].nombre); //Compara entre todos los usuarios
 						if (comp1 == 0) {
 							break;
+					
 						}
 					}
 
 					if (comp1 != 0) {
-						strcpy_s(usu[j].nombre, 50, aux[j].nombre); //guarda el nombre nuevo en usuarios
-						printf("Introduzca su contrasena\n");
-						scanf_s("%s", &usu[j].contrasena, 50);
-						printf("Introduzca su numero de cuenta bancaria\n");
-						scanf_s("%s", &usu[j].cuentabancaria, 25);
-						printf("Usted ha sido registrado como %s con contrasena %s y numero de cuenta %s\n", usu[j].nombre, usu[j].contrasena, usu[j].cuentabancaria);
-						Sleep(2000);
-
+						if (j == NUMUSUARIOS) {
+							printf("NO SE PUEDEN REGISTRAR MAS USUARIOS\n");
+							
+						}
+						else {
+							strcpy_s(usu[j].nombre, 50, aux.nombre); //guarda el nombre nuevo en usuarios
+							printf("Introduzca su contrasena\n");
+							scanf_s("%s", &usu[j].contrasena, 50);
+							printf("Introduzca su numero de cuenta bancaria\n");
+							scanf_s("%s", &usu[j].cuentabancaria, 25);
+							printf("Usted ha sido registrado como %s con contrasena %s y numero de cuenta %s\n", usu[j].nombre, usu[j].contrasena, usu[j].cuentabancaria);
+							Sleep(2000);
+							j = j++;
+						}
 					}
 					else {
 						printf("Ya existe un usuario con ese nombre, intente con otro\n");
@@ -102,10 +115,9 @@ void main() {
 				getchar();
 				break;
 			}
-			j = j++;
-	} while (j < NUMUSUARIOS &&  opcion !=3); //Se repite hasta que se acabe le numero de usuarios permitidos 
-	// o se decida salir del programa
 		
+	} while ( opcion !=3);//Se repite hasta que se desee salir del programa
+	
 	
 	system("pause");
 }
